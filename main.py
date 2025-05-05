73c09801cb3a9a91fe2e6572d0d7c636d70caafd
takumalight <martianburritos@gmail.com>
2025-05-05 11:27:08 -0500

    CH2-L6

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
-from circleshape import *
 from player import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
-
-    screen.fill("black")
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        screen.fill("black")
+        player.update(dt)
         player.draw(screen)
 
         # Flip must happen last
-        time_passed = pygame.time.Clock().tick(60)
-        dt = time_passed / 1000.0
+        dt = clock.tick(60) / 1000.0
         pygame.display.flip()
 
 if __name__ == "__main__":
