01318d6fae590449f4ed7285ce7a969c25beabd3
takumalight <martianburritos@gmail.com>
2025-05-05 11:15:09 -0500

    adding shape and player files

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,14 +12,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
+
+    screen.fill("black")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        player.draw(screen)
+
         # Flip must happen last
+        time_passed = pygame.time.Clock().tick(60)
+        dt = time_passed / 1000.0
         pygame.display.flip()
 
 if __name__ == "__main__":
