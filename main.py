a6453c4f173b1c979062ce6d84e5a25781f61bd5
takumalight <martianburritos@gmail.com>
2025-05-05 11:39:25 -0500

    CH2-L8

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # Groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updateable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
 
         # Flip must happen last
         dt = clock.tick(60) / 1000.0
